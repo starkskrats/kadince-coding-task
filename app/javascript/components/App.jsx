13d070b861b0e748c1c395c3f4959cf4fa9ae234
Andrew Starks <andrew@starks.me>
2021-10-25 01:03:02 -0600

    finished

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -4,134 +4,139 @@ import List from "../components/List";
 const App  = () => {
     const [todos, setTodos] = useState([]);
     const [todo, setTodo] = useState('');
+    const [filter, setFilter] = useState('All');
 
     useEffect(() => {
-        const url = "/todos/all_todos";
-        fetch(url)
-          .then(response => {
-            if (response.ok) {
-              return response.json();
-            }
-            throw new Error("Network response was not ok.");
-          })
-          .then(response => {
-            setTodos(response);
-          })
-          .catch(() => console.log('An error occurred while fetching the todos'));
-      }, []);
-
-      const handleChange = (event) => {
-        setTodo(event.target.value);
-      }
-
-      const handleUpdate = (body) => {
-        const url = "/todos/update";
-        const token = document.querySelector('meta[name="csrf-token"]').content;
+      const url = "/todos/all_todos";
+      fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then(response => {
+        setTodos(response);
+      })
+      .catch(() => console.log('An error occurred while fetching the todos'));
+    }, []);
     
-        fetch(url, {
-          method: "PUT",
-          headers: {
-            "X-CSRF-Token": token,
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify(body)
-        })
-          .then(response => {
-            if (response.ok) {
-              return response.json();
-            }
-            throw new Error("Network response was not ok.");
-          })
-          .then(response => {
-            setTodos(todos.map((todo) => todo.id === body.id ? {...todo, title: body.title, completed: body.completed} : todo));
-            return false;
-          })
-          .catch(() => console.log('An error occurred while updating the todo item'));
+    const handleChange = (event) => {
+      setTodo(event.target.value);
     }
 
-      const handleClick = (event) => {
-        event.preventDefault();
+    const handleUpdate = (body) => {
+      const url = "/todos/update";
+      const token = document.querySelector('meta[name="csrf-token"]').content;
     
-        if(todo === '') return;
+      fetch(url, {
+        method: "PUT",
+        headers: {
+          "X-CSRF-Token": token,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+      })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then(response => {
+        setTodos(todos.map((todo) => todo.id === body.id ? {...todo, title: body.title, completed: body.completed} : todo));
+        return false;
+      })
+      .catch(() => console.log('An error occurred while updating the todo item'));
+    }
     
-        const todoBody = {
-          title: todo,
-          completed: false
-        };
-        
-        const url = "/todos/create";
-        const token = document.querySelector('meta[name="csrf-token"]').content;
+    const handleClick = (event) => {
+      event.preventDefault();
     
-        fetch(url, {
-          method: "POST",
-          headers: {
-            "X-CSRF-Token": token,
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify(todoBody)
+      if(todo === '') return;
+    
+      const todoBody = {
+        title: todo,
+        completed: false
+      };
+      
+      const url = "/todos/create";
+      const token = document.querySelector('meta[name="csrf-token"]').content;
+      
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "X-CSRF-Token": token,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(todoBody)
+      })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
         })
-          .then(response => {
-            if (response.ok) {
-              return response.json();
-            }
-            throw new Error("Network response was not ok.");
-          })
-          .then(response => {
-            setTodos([...todos, response]);
-            document.getElementById('addTodo').value = '';
-            return false;
-          })
-          .catch(() => console.log('An error occurred while adding the todo item'));
-      }
-
-      const handleDelete = (body) => {
-        const url = "/todos/delete";
-        const token = document.querySelector('meta[name="csrf-token"]').content;
-    
-        fetch(url, {
-          method: "DELETE",
-          headers: {
-            "X-CSRF-Token": token,
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify(body)
+        .then(response => {
+          setTodos([...todos, response]);
+          document.getElementById('addTodo').value = '';
+          return false;
         })
-          .then(response => {
-            if (response.ok) {
-              return response.json();
-            }
-            throw new Error("Network response was not ok.");
-          })
-          .then(response => {
-            setTodos(todos.filter((todo) => body.id !== todo.id))
-            return false;
-          })
-          .catch(() => console.log('An error occurred while adding the todo item'));
-      }
+      .catch(() => console.log('An error occurred while adding the todo item'));
+    }
+    
+    const handleDelete = (body) => {
+      const url = "/todos/delete";
+      const token = document.querySelector('meta[name="csrf-token"]').content;
+    
+      fetch(url, {
+        method: "DELETE",
+        headers: {
+          "X-CSRF-Token": token,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+      })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then(response => {
+        setTodos(todos.filter((todo) => body.id !== todo.id))
+        return false;
+      })
+      .catch(() => console.log('An error occurred while adding the todo item'));
+    }
 
-return (
-<div className="container">
-  <h1>Todo List</h1>
-  <form>
-            <h2>Enter a task to add to the list</h2>
-            <div className="input-group mb-3">
-                <input type="text" id="addTodo" className="form-control" onChange={handleChange} />
-                <button className="btn btn-primary" type="button" onClick={handleClick}>Add</button>
-            </div>
+    const handleFilterChange = (event) => {
+      setFilter(event.target.value);
+    }
+
+    return (
+      <div className="container">
+        <h1>Todo List</h1>
+        <form>
+          <h2>Enter a task to add to the list</h2>
+          <div className="input-group mb-3">
+            <input type="text" id="addTodo" className="form-control" onChange={handleChange} />
+            <button className="btn btn-primary" type="button" onClick={handleClick}>Add</button>
+          </div>
         </form>
-  <div className="row mb-3">
-      <div className="btn-group" role="group">
-        <input type="radio" className="btn-check" name="filter" id="all" autoComplete="off" />
-        <label className="btn btn-outline-primary" htmlFor="all">All</label>
-        <input type="radio" className="btn-check" name="filter" id="pending" autoComplete="off" />
-        <label className="btn btn-outline-primary" htmlFor="pending">Pending</label>
-        <input type="radio" className="btn-check" name="filter" id="complete" autoComplete="off" />
-        <label className="btn btn-outline-primary" htmlFor="complete">Complete</label>
+        <div className="row mb-3">
+          <div className="btn-group" role="group">
+            <input type="radio" className="btn-check" name="filter" value="All" onChange={handleFilterChange} id="All" />
+            <label className="btn btn-outline-primary" htmlFor="All">All</label>
+            <input type="radio" className="btn-check" name="filter" value="Pending" onChange={handleFilterChange} id="Pending" />
+            <label className="btn btn-outline-primary" htmlFor="Pending">Pending</label>
+            <input type="radio" className="btn-check" name="filter" value="Completed" onChange={handleFilterChange} id="Complete" />
+            <label className="btn btn-outline-primary" htmlFor="Complete">Complete</label>
+          </div>
+        </div>
+        <List todoList={todos} handleUpdate={handleUpdate} handleDelete={handleDelete} filter={filter} />
       </div>
-  </div>
-  <List todoList={todos} handleUpdate={handleUpdate} handleDelete={handleDelete} />
-  </div>
-  )
+    )
 }
 
 export default App;
--- app/javascript/components/List.jsx
+++ app/javascript/components/List.jsx
@@ -1,16 +1,24 @@
 import React from "react"
 import TodoItem from "./TodoItem";
 
-const List = ({ todoList, handleUpdate, handleDelete }) => {
-    return (
-        <div>
-          {todoList.map((todo) => {
-        return (
-          <TodoItem key={todo.id} todo={todo} handleSubmit={handleUpdate} handleDelete={handleDelete} />
-        )
-      })}
-        </div>
-    );
+const FILTER_MAP = {
+  All: () => true,
+  Pending: todo => !todo.completed,
+  Completed: todo => todo.completed
+};
+
+const List = ({ todoList, handleUpdate, handleDelete, filter}) => {
+
+  const filteredList = todoList
+                        .filter(FILTER_MAP[filter])
+                        .map(todo => 
+                        (<TodoItem 
+                          key={todo.id} 
+                          todo={todo} 
+                          handleSubmit={handleUpdate} 
+                          handleDelete={handleDelete} 
+                        />));
+  return (<div>{filteredList}</div>);
 }
 
 export default List;
